1c2791780506fd9795af8237c10224c7ffdb1cae
Anwesh Mohanty <58528894+anweshsbp@users.noreply.github.com>
2020-07-07 09:54:23 +0530

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,6 +1,6 @@
 #consumer key, consumer secret, access token, access secret.
-ckey=""
-csecret=""
+ckey="tUgkNrAfz45yvABii9yKofJQE"
+csecret="ptNgnz78tgayfluvWrCySLwnBdhHYQatJMopocWRZh1Vnre0P0"
 
-atoken=""
-asecret=""
+atoken="1219626370684407809-IUGNv7j9uPRzrxYkIthFqLBrdGo8mo"
+asecret="k9LRJPskpSKTcFynq2AGOWwwLdDGe4Ma3GfqJAefCiF7L"
